6179e9fe49e8c631d3de3f8bfc5160e8dd069390
Josh Talbot <jmtalbot00@gmail.com>
2023-12-04 13:53:18 -0700

    fixed form formatting, added more .css

--- client/src/Portfolio.jsx
+++ client/src/Portfolio.jsx
@@ -9,14 +9,19 @@ function Portfolio() {
                 <div>
                     Add a Stock to a portfolio:
                 </div>
-                <label>
-                    NYSE code:
-                    <input type="text" placeholder="ex: AAPL"/>
-                </label>
+                <div>
+                    <label>
+                        NYSE Stock Ticker Symbol:
+                        <input type="text" placeholder="ex: AAPL"/>
+                    </label>
+                </div>
+                <div>
                 <label>
                     How many Shares?
                     <input type="number" min={1} max={9999}/>
                 </label>
+                </div>
+                
                 <button>Add Stock to Portfolio</button>
             </form>
         </>
