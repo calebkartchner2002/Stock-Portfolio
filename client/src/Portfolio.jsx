feca82b3dcaafe54403b9e2c0966a0bebde6821f
Josh Talbot <jmtalbot00@gmail.com>
2023-12-04 13:40:44 -0700

    started working on portfolio page, edited jsx file to make add stock form & added .css file

--- client/src/Portfolio.jsx
+++ client/src/Portfolio.jsx
@@ -1,7 +1,24 @@
+import './Portfolio.css';
+
+
 function Portfolio() {
     return (
         <>
-            <div>welcome to the portfolio page</div>
+            <div class="header">Portfolio Page</div>
+            <form class="container">
+                <div>
+                    Add a Stock to a portfolio:
+                </div>
+                <label>
+                    NYSE code:
+                    <input type="text" placeholder="ex: AAPL"/>
+                </label>
+                <label>
+                    How many Shares?
+                    <input type="number" min={1} max={9999}/>
+                </label>
+                <button>Add Stock to Portfolio</button>
+            </form>
         </>
     )
 }
