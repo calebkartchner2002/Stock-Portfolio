5406dad5e2c4c3cf5a5848f7261f27302577fd30
Josh Talbot <jmtalbot00@gmail.com>
2023-12-11 12:32:10 -0700

    added urls

--- _server/core/urls.py
+++ _server/core/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', view=views.index, name="index"),
     path('me/', view=views.me, name="current user"),
     path('prevClose/', view=views.getCloseInformation, name='prevClose'),
+    path('makeTrade/', view=views.makeTrade, name='make Trade'),
+    path('removeTrade/', view=views.removeTrade, name="remove Trade"),
 ]
